b7746572d83eed30f26c78fd9a42a7efa5583503
bys0kbata <92207641+bys0kbata@users.noreply.github.com>
2023-03-12 23:38:12 +0400

    Initial commit

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+/**
+ * Sample Skeleton for 'hello-view.fxml' Controller Class
+ */
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class HelloController {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+    @FXML
+    public static char onePyth;
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+
+    }
+
+}
+
